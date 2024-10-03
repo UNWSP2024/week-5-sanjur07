1a98bdb6e17e754d59d8d16ed38119bf6fb74751
Sanju <sanjanarnath07@gmail.com>
2024-10-03 08:13:14 -0500

    Sanjana Raveendranath - 10/3/2024

--- program_1.py
+++ program_1.py
@@ -5,16 +5,13 @@
 # The conversion must be done as a function with input and output.
 
 
-def kilometer_conversion(kilometers):    
+def kilometer_conversion():    
     miles = 0.0
     ######################
     miles = kilometers * 0.6214
     return miles
     ######################    
 
-
-    # Return the variable to the calling function
-    return miles
 
 #### This piece of the code has been done for you,
 #### you only need to worry about the actual temp 
@@ -24,6 +21,6 @@ if __name__ == '__main__':
     print('in main')
     # Call kilometer_conversion
     kilometers = float(input("Enter distance in kilometers: "))
-    final = kilometer_conversion(kilometers)
+    final = kilometer_conversion()
     print(f"The distance in miles is: {final:.2f}")
     # Display the miles
